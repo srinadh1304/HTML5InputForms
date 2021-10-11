987adac3a82ddb04fe6ca107acd43ff4ea6228e9
srinadh1304 <srinadh1304@gmail.com>
2021-10-11 07:22:29 +0530

    MODIFIED: added event listener for invalid email

--- js/simple_form.js
+++ js/simple_form.js
@@ -2,6 +2,8 @@ const NAME_REGEX = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 
+const EMAIL_REGEX = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
+
 text.addEventListener('input', function() {
     if (NAME_REGEX.test(text.value)) {
         textError.textContent = "";
@@ -16,3 +18,12 @@ output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 })
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function() {
+    if (EMAIL_REGEX.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Invalid Email";
+    }
+});
